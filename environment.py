2b85d61e34c75d4f97e3d3f7e3a3b48a35ce7dad
Matthieu Sarkis <matthieu.sarkis@gmail.com>
2021-05-16 14:37:59 +0900

    environment more modular

--- environment.py
+++ environment.py
@@ -5,92 +5,106 @@ import gym
 class Environment(gym.Env):
 
     def __init__(self, 
-                 df: pd.DataFrame, 
-                 stock_space_dimension: int,                
+                 stock_market_history: pd.DataFrame,                
                  initial_cash_in_bank: float,
                  buy_rate: float,
                  sell_rate: float,
                  sac_temperature: float,
-                 state_space_dimension: int,
-                 action_space_dimension: int,
                  action_scale: float = 50,
                  ) -> None:
         
         super(Environment, self).__init__()
         
-        self.df = df
-        self.stock_space_dimension = stock_space_dimension
-        self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(state_space_dimension,))
-        self.action_space = gym.spaces.Box(low=-1, high=1, shape=(action_space_dimension,)) 
+        self.stock_market_history = stock_market_history
+        self.time_horizon, self.stock_space_dimension = stock_market_history.shape
+        
+        self.state_space_dimension = 2 * self.stock_space_dimension + 1
+        self.action_space_dimension = self.stock_space_dimension
+        
+        self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(self.state_space_dimension,))
+        self.action_space = gym.spaces.Box(low=-1, high=1, shape=(self.action_space_dimension,)) 
         self.action_scale = action_scale
+        
         self.initial_cash_in_bank = initial_cash_in_bank
+        
         self.buy_rate = buy_rate
         self.sell_rate = sell_rate
         self.sac_temperature = sac_temperature
         
         self.current_step = None
-        self.data = None
-        self.state = None
-        self.reward = None
+        self.cash_in_bank = None
+        self.stock_prices = None
+        self.number_of_shares = None
 
         self.reset()
     
-    def reset(self) -> list[float]:  
+    def reset(self) -> np.array:  
         
-        self.state = [self.initial_cash_in_bank] + self.data.Close.values.tolist() + [0] * self.stock_space_dimension
         self.current_step = 0
-        self.data = self.df.loc[self.current_step,:]
-        self.reward = 0
-        return self.state
-    
-    def step(self, 
-             actions: np.array) -> tuple[list[float], float, bool, dict]:
+        self.cash_in_bank = self.initial_cash_in_bank
+        self.stock_prices = self.stock_market_history[self.current_step]
+        self.number_of_shares = np.zeros(self.stock_space_dimension)
         
-        done = self.current_step == len(self.df.index.unique()) - 1
+        return self._get_observation()
+        
+    def step(self, 
+             actions: np.array) -> tuple[np.array, float, bool, dict]:
+        
         self.current_step += 1
+        done = (self.current_step == self.time_horizon - 1)
         
         actions = (actions * self.action_scale).astype(int)
         sorted_indices = np.argsort(actions)
+                
+        initial_value_portfolio = self._get_portfolio_value()
         
-        initial_value_portfolio = self.state[0] + sum(np.array(self.state[1:(self.stock_space_dimension+1)]) * np.array(self.state[(self.stock_space_dimension+1):(self.stock_space_dimension*2+1)]))
-        
-        sell_idx = sorted_indices[:np.where(actions < 0)[0].shape[0]]
-        buy_idx = sorted_indices[::-1][:np.where(actions > 0)[0].shape[0]]
+        sell_idx = sorted_indices[ : np.where(actions < 0)[0].shape[0]]
+        buy_idx = sorted_indices[::-1][ : np.where(actions > 0)[0].shape[0]]
 
         for idx in sell_idx:  
             self._sell(idx, actions[idx])
             
         for idx in buy_idx: 
             self._buy(idx, actions[idx])
+                   
+        new_value_portfolio = self._get_portfolio_value()
+        info = {'value_portfolio': new_value_portfolio}
+        reward = (new_value_portfolio - initial_value_portfolio) * self.sac_temperature         
 
-        self.data = self.df.loc[self.current_step, :]    
-        self.state = [self.state[0]] + self.data.Close.values.tolist() + list(self.state[(self.stock_space_dimension+1):(self.stock_space_dimension*2+1)])
-                        
-        value_portfolio = self.state[0] + sum(np.array(self.state[1:(self.stock_space_dimension+1)]) * np.array(self.state[(self.stock_space_dimension+1):(self.stock_space_dimension*2+1)]))
-        info = {'value_portfolio': value_portfolio}
-        
-        self.reward = (value_portfolio - initial_value_portfolio) * self.sac_temperature         
-
-        return self.state, self.reward, done, info
+        return self._get_observation(), reward, done, info
         
     def _sell(self, 
               idx: int, 
               action: int) -> None:
     
-        if self.state[idx+self.stock_space_dimension+1] <= 0:
+        if self.number_of_shares[idx] <= 0:
             return
          
-        n_stocks_to_sell = min(-action, self.state[idx+self.stock_space_dimension+1])
-        money_inflow = self.state[idx+1] * n_stocks_to_sell * (1 - self.sell_rate)
-        self.state[0] += money_inflow
-        self.state[idx+self.stock_space_dimension+1] -= n_stocks_to_sell
+        n_stocks_to_sell = min(-action, self.number_of_shares[idx])
+        money_inflow = n_stocks_to_sell * self.stock_prices[idx] * (1 - self.sell_rate)
+        self.cash_in_bank += money_inflow
+        self.number_of_shares[idx] -= n_stocks_to_sell
             
     def _buy(self, 
              idx: int, 
              action: int) -> None:
         
-        n_stocks_to_buy = min(action, self.state[0] // self.state[idx+1])
-        money_outflow = self.state[idx+1] * n_stocks_to_buy * (1 + self.buy_rate)
-        self.state[0] -= money_outflow
-        self.state[idx+self.stock_space_dimension+1] += n_stocks_to_buy   
+        n_stocks_to_buy = min(action, self.cash_in_bank // self.stock_prices[idx])
+        money_outflow = n_stocks_to_buy * self.stock_prices[idx] * (1 + self.buy_rate)
+        self.cash_in_bank -= money_outflow
+        self.number_of_shares[idx] += n_stocks_to_buy   
+        
+    def _get_observation(self) -> np.array:
+        
+        observation = np.empty(self.state_space_dimension)
+        observation[0] = self.cash_in_bank
+        observation[1 : self.stock_space_dimension+1] = self.stock_prices
+        observation[self.stock_space_dimension+1 : ] = self.number_of_shares
+        
+        return observation
+    
+    def _get_portfolio_value(self) -> float:
+        
+        portfolio_value = self.cash_in_bank + self.number_of_shares.dot(self.stock_prices)
+        return portfolio_value
         
