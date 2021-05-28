c3ca98c81fda2575eeda21e42ac654629518fbb7
Matthieu Sarkis <matthieu.sarkis@gmail.com>
2021-05-28 14:05:08 +0900

    Minor

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(
-   name='Soft Actor Critic Trading Bot',
+   name='Soft Actor Critic Bot',
    version='1.0',
-   description='Soft Actor Critic Trading Bot',
+   description='Soft Actor Critic Bot',
    license="Apache License 2.0",
    long_description=long_description,
    author='Matthieu Sarkis',
