fc145dfb14415b3c572489108f8eaa60c5f91fee
Matthieu Sarkis <matthieu.sarkis@gmail.com>
2021-06-05 01:33:10 +0900

    comments environment class

--- src/environment.py
+++ src/environment.py
@@ -13,11 +13,18 @@
 import gym
 import numpy as np
 import pandas as pd
-import time
 from typing import Tuple
 
 from src.utilities import append_corr_matrix
 class Environment(gym.Env):
+    """Environment for stock trading.
+    
+    Inherits from gym.Env, to access gym.spaces.Box for the state space and action space.
+    
+    Attributes:
+        observation_space (gym.spaces.Box): (bank account balance, stocks price, corr matrix, owned shares)
+        action_space (gym.spaces.Box): cube [-1,1]^n_stocks. positive value: buy, negative value: sale
+    """
 
     def __init__(self, 
                  stock_market_history: pd.DataFrame,                
@@ -30,6 +37,23 @@ class Environment(gym.Env):
                  use_corr_matrix: bool = False,
                  window: int = 20,
                  ) -> None:
+        """Initialize the Environment object.
+
+        Args:
+            stock_market_history (pd.DataFrame): (time_horizon * number_stocks) format                
+            initial_cash_in_bank (float): Initial amount of money in the bank
+            buy_rate (float): fees in percentage for buying a stock 
+            sell_rate (float): fees in percentage for selling a stock 
+            bank_rate (float): interest rate of the bank account
+            limit_n_stocks (float): maximum number of stocks one can buy or sell at once
+            buy_rule (str): specifies the order in which one buys the stocks the agent decided to buy
+            use_corr_matrix (bool): whether or not to append the correlation matrix to the time series
+            window (int): in case the correlation matrix is used, size of the sliding window
+
+        Returns:
+            no value
+        """
+        
         
         super(Environment, self).__init__()
         
@@ -66,6 +90,12 @@ class Environment(gym.Env):
         self.reset()
     
     def reset(self) -> np.array:  
+        """initialize the bank account balance and stock market status 
+        the initial time.
+        
+        Returns:
+            np.array of the initial observation 
+        """
         
         self.current_step = 0
         self.cash_in_bank = self.initial_cash_in_bank
@@ -77,6 +107,15 @@ class Environment(gym.Env):
     def step(self, 
              actions: np.array,
              ) -> Tuple[np.array, float, bool, dict]:
+        """Take one step in the trading environment.
+        
+        Args:
+            actions (np.array): continuous action chosen buy the agent
+            
+        Returns:
+            np.array for the new state
+        """
+        
         
         self.current_step += 1      
         initial_value_portfolio = self._get_portfolio_value()
@@ -96,6 +135,21 @@ class Environment(gym.Env):
     def _trade(self, 
                actions: np.array,
                ) -> None:
+        """Perform one trade according to the actions decided buy an agent
+        
+        Various buying rules are implemented:
+        -most_first: buy the stocks with which most shares are to be bought first 
+                    since the agent thinks they show an interesting trend
+        -cyclic
+        -random
+        First sell, then buy.
+        
+        Args:
+            actions (np.array): continuous action chosen buy the agent
+            
+        Returns:
+            no value
+        """
         
         actions = (actions * self.limit_n_stocks).astype(int)
         sorted_indices = np.argsort(actions)
@@ -130,6 +184,15 @@ class Environment(gym.Env):
               idx: int, 
               action: int,
               ) -> None:
+        """Sell the required amount of stocks, if anything to be sold.
+        
+        Args:
+            idx (int): index of the stock to be sold
+            action (int): number of shares of the stock to be sold
+            
+        Returns:
+            no value
+        """
     
         if int(self.number_of_shares[idx]) < 1:
             return
@@ -141,8 +204,17 @@ class Environment(gym.Env):
             
     def _buy(self, 
              idx: int, 
-             action: int,
+             action: int = 1,
              ) -> None:
+        """Buy the required amount of stocks, if enough money in the bank account.
+        
+        Args:
+            idx (int): index of the stock to be bought
+            action (int): number of shares of the stock to be bought, defaulted at 1 for convenience
+            
+        Returns:
+            no value
+        """
         
         if self.cash_in_bank < 0:
             return
@@ -153,6 +225,11 @@ class Environment(gym.Env):
         self.number_of_shares[idx] += n_stocks_to_buy   
         
     def _get_observation(self) -> np.array:
+        """Observation in the format given by the state_space, and perceived by the agent.
+        
+        Returns:
+            np.array for the observation
+        """
         
         observation = np.empty(self.state_space_dimension)
         observation[0] = self.cash_in_bank
@@ -162,6 +239,11 @@ class Environment(gym.Env):
         return observation
     
     def _get_portfolio_value(self) -> float:
+        """Performs the scalar product of the owned shares and the stock prices and add the bank account.
+        
+        Returns:
+            np.array for the total portfolio value
+        """
         
         portfolio_value = self.cash_in_bank + self.number_of_shares.dot(self.stock_prices[:self.stock_space_dimension])
         return portfolio_value
