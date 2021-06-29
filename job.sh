85dfd2a5b538d3b52fe1bd24482c0dad37b0e821
Matthieu Sarkis <matthieu.sarkis@gmail.com>
2021-06-29 16:14:14 +0900

    simplify job.sh

--- job.sh
+++ job.sh
@@ -13,31 +13,44 @@
 
 #========================================================
 
+# Simple command line: In case you trust the default values I gave to the various params/hyperparams, cf. main.py
+
 #srun --unbuffered 
 python src/main.py \
---initial_cash 10000000 \
---buy_rate 0.0001 \
---sell_rate 0.0001 \
---bank_rate 0.5 \
---sac_temperature 1.0 \
---limit_n_stocks 100 \
---lr_Q 0.0003 \
---lr_pi 0.0003 \
---lr_alpha 0.0003 \
---gamma 0.99 \
---tau 0.005 \
---batch_size 256 \
---layer_size 256 \
---n_episodes 1000 \
---seed 0 \
---delay 2 \
 --mode train \
---memory_size 1000000 \
---initial_date 2015-01-01 \
---final_date 2020-12-31 \
---gpu_devices 0 1 2 3 \
---grad_clip 2.0 \
---buy_rule most_first \
+--initial_cash 10000000 \
+--n_episodes 1000 \
 --agent_type distributional \
---window 40 \
---use_corr_matrix \
+--use_corr_eigenvalues \
+
+
+# Detailed command line: In case you want to tune each param/hyperparam manually
+
+#srun --unbuffered 
+#python src/main.py \
+#--initial_cash 10000000 \
+#--buy_rate 0.0001 \
+#--sell_rate 0.0001 \
+#--bank_rate 0.5 \
+#--sac_temperature 1.0 \
+#--limit_n_stocks 100 \
+#--lr_Q 0.0003 \
+#--lr_pi 0.0003 \
+#--lr_alpha 0.0003 \
+#--gamma 0.99 \
+#--tau 0.005 \
+#--batch_size 256 \
+#--layer_size 256 \
+#--n_episodes 1000 \
+#--seed 0 \
+#--delay 2 \
+#--mode train \
+#--memory_size 1000000 \
+#--initial_date 2015-01-01 \
+#--final_date 2020-12-31 \
+#--gpu_devices 0 1 2 3 \
+#--grad_clip 2.0 \
+#--buy_rule most_first \
+#--agent_type distributional \
+#--window 40 \
+#--use_corr_matrix \
